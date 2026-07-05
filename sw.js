fd5e58a0e6a85493adff7b0fb4e5ea1f79b2c6f0
princemcuwuigiaren-hub <princemcuwuigiaren@gmail.com>
2026-07-05 18:15:08 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'surveyor-v1';
+const CACHE_NAME = 'surveyor-v2';
 const ASSETS = ['./', './surveyor.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
