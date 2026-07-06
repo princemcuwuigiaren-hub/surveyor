807fc71a5a18165e0a799bd5ef0afa3cf6fe9c13
princemcuwuigiaren-hub <princemcuwuigiaren@gmail.com>
2026-07-06 07:45:46 +0100

    Update CACHE_NAME to surveyor-v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'surveyor-v4';
+const CACHE_NAME = 'surveyor-v5';
 const ASSETS = ['./', './surveyor.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
